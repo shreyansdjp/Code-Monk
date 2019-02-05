833777ad3cd6f89365558a49ac2111d87eae740d
Shreyans Jain <shreyansjain68@gmail.com>
2019-02-05 07:00:46 +0530

    completed javascript solution

--- JavaScript/Basics Of Programming/basicsOfInputOutput.js
+++ JavaScript/Basics Of Programming/basicsOfInputOutput.js
@@ -1,24 +1,29 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 process.stdin.resume();
 process.stdin.setEncoding("utf-8");
 
-var stdin_input = [];
-var inputCounter = 0;
-process.stdin.on('data', function (input) {
-    inputCounter++;
-    stdin_input.push(input);
-    if (inputCounter == 2) {
-        process.exit();
-    }
-});
+function takeInput(done) {
+    var inputCounter = 0;
+    var stdin_input = [];
+    rl.on('line', function (input) {
+        stdin_input.push(input);
+        inputCounter++;
+        if (inputCounter == 2) {
+            done(stdin_input);
+            process.exit();
+        }
+    })
+}
 
-process.on('exit', function () {
-    var number = parseInt(stdin_input[0]);
-    var string = stdin_input[1];
-    number *= 2;
-    /*
-    process.stdout.write(number.toString() + '\n');
-    process.stdout.write(string);
-    */
-    console.log(number);
+takeInput(function (input) {
+    var number = parseInt(input[0]);
+    var string = input[1];
+    console.log(number*2);
     console.log(string);
-});
+}); 
